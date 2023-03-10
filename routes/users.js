6eb3ce1573b6bebc3be7f46a8668aacc9b61fc7a
unknown <manukrishnaap@gmail.com>
2023-03-10 13:52:52 +0530

    09/03/2023

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('customer/home',{custeroute:true})
+  res.render('customer/cust_home',{custeroute:true})
+});
+
+router.get('/cart', function(req, res, next) {
+  res.render('customer/cart',{custeroute:true})
 });
 module.exports = router;

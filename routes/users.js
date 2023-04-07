216e93cdd8049430cde2dd7eb187f5d66ab4105b
unknown <manukrishnaap@gmail.com>
2023-04-07 13:45:04 +0530

    07-04-2023

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ var express = require('express');
 const fn = require('../mongodb/customer_help');
 const { log } = require('handlebars');
 const { ath } = require('../auth');
+const db = require('../config/connection');
+const { ObjectId } = require('mongodb');
+const async = require('hbs/lib/async');
+const { redirect } = require('express/lib/response');
 var router = express.Router();
 
 //form actions
@@ -15,17 +19,22 @@ router.post('/checkout_post', function(req, res, next) {
 
 /* GET users listing. */
 
-router.get('/',ath, function(req, res, next) {
+router.get('/',ath, async function(req, res, next) {
+  let data=await db.collection('farmer_products').find().toArray()
   console.log(req.session.loginStatus);
   console.log(req.session.userid);
   if(req.session.loginStatus){
-  res.render('customer/cust_home',{custeroute:true})}
+  res.render('customer/cust_home',{custeroute:true,data})}
   else{
     res.redirect('/login')
   }
 });
-router.get('/cart',ath, function(req, res, next) {
-  res.render('customer/cart',{custeroute:true})
+router.get('/cart',ath, async function(req, res, next) {
+  console.log(req.session.userid);
+  let u=req.session.userid
+  let data1= await db.collection('cart').findOne({userid:u})
+  console.log(data1);
+  res.render('customer/cart',{custeroute:true,data1})
 });
 
 router.get('/orders',ath, function(req, res, next) {
@@ -37,4 +46,16 @@ router.get('/book',ath, function(req, res, next) {
 router.get('/checkout',ath, function(req, res, next) {
   res.render('customer/checkout',{custeroute:true})
 });
+router.get('/booking/:id',ath, async function(req, res, next) {
+  const objectId=new ObjectId (req.params.id)  
+  let data=await db.collection('farmer_products').findOne({_id:objectId})
+  console.log(data);
+  let data1=req.session.userid
+  res.render('customer/cust_book',{custeroute:true,data,data1})
+});
+router.post('/cart_post',async function(req,res,next){
+  await db.collection('cart').insertOne(req.body)
+  res.redirect('/user/cart')
+})
+
 module.exports = router;

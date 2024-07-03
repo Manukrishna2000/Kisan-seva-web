2f45d03f06a88ad8efb38edf2099cceca75f2235
Manu Krishna A P <96103298+Manukrishna2000@users.noreply.github.com>
2024-07-03 10:16:16 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect('mongodb+srv://manukrishnaap:manukrishnaap>@cluster0.8synd0q.mongodb.net/kisan?retryWrites=true&w=majority&appName=Cluster0/kisan');
+mongoose.connect('mongodb+srv://manukrishnaap:tMOJ6kuNmujSZj3I>@cluster0.8synd0q.mongodb.net/kisan?retryWrites=true&w=majority&appName=Cluster0/kisan');
 
 let db=mongoose.connection
 module.exports = db
